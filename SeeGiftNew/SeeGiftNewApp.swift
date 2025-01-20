320c44948feca1b806132da981f65948fb478551
cdrogers13 <chrisdrogers1@gmail.com>
2025-01-20 15:53:38 -0500

    Initial Commit

---
+++ SeeGiftNew/SeeGiftNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeGiftNewApp.swift
+//  SeeGiftNew
+//
+//  Created by Chris Rogers on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SeeGiftNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SeeGiftNewTests/SeeGiftNewTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeeGiftNewTests.swift
+//  SeeGiftNewTests
+//
+//  Created by Chris Rogers on 1/20/25.
+//
+
+import Testing
+@testable import SeeGiftNew
+
+struct SeeGiftNewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
