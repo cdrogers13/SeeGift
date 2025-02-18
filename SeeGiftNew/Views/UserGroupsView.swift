e2765655502f69aca4fc92731f4994baefa4f659
cdrogers13 <chrisdrogers1@gmail.com>
2025-02-18 11:52:49 -0500

    Small visual change

--- SeeGiftNew/Views/UserGroupsView.swift
+++ SeeGiftNew/Views/UserGroupsView.swift
@@ -16,33 +16,12 @@ struct UserGroupsView: View {
                     Button(action: {
                         
                     }) {
-                        ZStack(alignment: .topTrailing) {
-                            HStack{
-                                //                                Text("#\(gift.ranking)")
-//                                Image(group.groupImage).resizable().cornerRadius(50).scaledToFit().frame(width: 200, height: 200)
-                                Spacer()
-                                VStack (alignment: .trailing){
-                                    Text(group.groupName)
-//                                    Text(gift.price, format: .currency(code: "USD"))
-                                    //                                    Button(action: {
-                                    //                                        currGift = gift
-                                    //                                        showDescPopup.toggle()
-                                    //                                        showList.toggle() //List and description popup should always be opposites
-                                    //                                        print("Test?")
-                                    //                                    }, label: {
-                                    //                                        Text("Details")
-                                    //                                    }).buttonStyle(BorderlessButtonStyle())
-                                }
-                            }.padding()
-                            //Text("#\(gift.ranking)").padding()
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 60, style: .continuous).frame(width: 350, height: 300)
+                            Text(group.groupName).font(.headline).foregroundColor(.black)
                         }
-                        
-                    }.background(Color.yellow).foregroundStyle(.black).clipShape(RoundedRectangle(cornerRadius: 30))
+                    }.background(.black).foregroundStyle(.yellow).clipShape(RoundedRectangle(cornerRadius: 30))
                 }.listRowBackground(Color.black)
-                //Color.black.ignoresSafeArea(.all)
-//                NavigationLink (destination: AddGiftView(createdUser)){
-//                    Label("Add New Gift", systemImage: "plus.app").font(.headline).imageScale(.large)
-//                }.padding([.bottom])
             }.padding()
         }
     }
