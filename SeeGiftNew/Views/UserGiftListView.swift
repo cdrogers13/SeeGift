f175eeac358c39a4ee9c33019518404830ebfe03
cdrogers13 <chrisdrogers1@gmail.com>
2025-03-12 22:08:45 -0400

    Cleaned up code. Fixed weirdness with hit testing being off but still being able to press last button in scrollview

--- SeeGiftNew/Views/UserGiftListView.swift
+++ SeeGiftNew/Views/UserGiftListView.swift
@@ -24,65 +24,55 @@ struct UserGiftListView: View {
         VStack{
             
             if (showDescPopup) {
-                GiftDetailView(showList: $showList, showDescPopup: $showDescPopup, gift: $currGift, /*giftList: $currUser.giftsList, */currFavGiftIndex: $currFavGiftIndex)
+                GiftDetailView(showList: $showList, showDescPopup: $showDescPopup, gift: $currGift, currFavGiftIndex: $currFavGiftIndex)
             }
             if (showList) {
                 NavigationView {
                     ZStack {
                         VStack {
-                        ScrollView (showsIndicators: false) {
-                            //                        ForEach(newList) {gift in
-                            ForEach(currUser.giftsList) {gift in
-                                Button(action: {
-                                    currGift = gift
-                                    currFavGiftIndex = currUser.giftsList.firstIndex(where: {
-                                        /*currFavGiftIndex = newList.firstIndex(where: {*/ $0.isMostWanted }) ?? 0
-                                    showDescPopup.toggle()
-                                    showList.toggle() //List and description popup should always be opposites
-                                }) {
-                                    ZStack(alignment: .topTrailing) {
-                                        HStack{
-                                            if (gift.baseImage.isEmpty) {
-                                                defaultGiftImage
+                            ScrollView (showsIndicators: false) {
+                                ForEach(currUser.giftsList) {gift in
+                                    Button(action: {
+                                        currGift = gift
+                                        currFavGiftIndex = currUser.giftsList.firstIndex(where: {
+                                            $0.isMostWanted }) ?? 0
+                                        showDescPopup.toggle()
+                                        showList.toggle() //List and description popup should always be opposites
+                                    }) {
+                                        ZStack(alignment: .topTrailing) {
+                                            HStack{
+                                                if (gift.baseImage.isEmpty) {
+                                                    defaultGiftImage
+                                                }
+                                                else {
+                                                    Image(gift.baseImage).resizable().cornerRadius(50).scaledToFit().frame(width: 200, height: 200)
+                                                }
+                                                Spacer()
+                                                VStack (alignment: .trailing){
+                                                    Text(gift.name)
+                                                    Text(gift.price, format: .currency(code: "USD"))
+                                                }
+                                            }.padding()
+                                            if (gift.isMostWanted) {
+                                                Image(systemName: "star.fill").resizable().frame(width: 20, height: 20).padding()
                                             }
-                                            else {
-                                                Image(gift.baseImage).resizable().cornerRadius(50).scaledToFit().frame(width: 200, height: 200)
-                                            }
-                                            Spacer()
-                                            VStack (alignment: .trailing){
-                                                Text(gift.name)
-                                                Text(gift.price, format: .currency(code: "USD"))
-                                            }
-                                        }.padding()
-                                        if (gift.isMostWanted) {
-                                            Image(systemName: "star.fill").resizable().frame(width: 20, height: 20).padding()
                                         }
-                                    }
-                                }.allowsHitTesting(showAddGiftPopup ? false : true).background(Color.yellow).foregroundStyle(.black).clipShape(RoundedRectangle(cornerRadius: 30))
-                            }.listRowBackground(Color.black)
-                            //Color.black.ignoresSafeArea(.all)
-                            //                        NavigationLink (destination: AddGiftView(createdUser)){
-                            //                            NavigationLink (destination: AddGiftViaLinkView()){
-                            //                                Label("Add New Gift", systemImage: "plus.app").font(.headline).imageScale(.large)
-                            //                            }.padding([.bottom])
-                            Button(action: {
-                                showAddGiftPopup.toggle()
-                            })
-                            {
-                                Label(showAddGiftPopup ? "Cancel" : "Add New Gift", systemImage: "plus.app").font(.headline).imageScale(.large)
-                            }
-                        }.padding().blur(radius: showAddGiftPopup ? 15 : 0).onTapGesture {
-                            showAddGiftPopup = false
-                        }
-                            Button(action: {
+                                    }.background(Color.yellow).foregroundStyle(.black).clipShape(RoundedRectangle(cornerRadius: 30))
+                                }.allowsHitTesting(showAddGiftPopup ? false : true).listRowBackground(Color.black)
+                                //Color.black.ignoresSafeArea(.all)
                                 
-                            })
-                            {
-                                Image(systemName: "plus").resizable().frame(width: 50, height: 50).symbolVariant(.circle.fill).imageScale(.large)
-//                                Label{ Text("New Gift").font(.headline) }
-//                                icon: {Image(systemName: "plus").symbolVariant(.circle.fill).imageScale(.large)}.labelStyle(VerticalLabelStyle(textColor: .white, picColor: .yellow))
+                                Button(action: {
+                                    showAddGiftPopup.toggle()
+                                })
+                                {
+                                    Image(systemName: "plus").resizable().frame(width: 50, height: 50).symbolVariant(.circle.fill).imageScale(.large)
+                                }
+                                
+                            }.padding().blur(radius: showAddGiftPopup ? 15 : 0).onTapGesture {
+                                showAddGiftPopup = false
                             }
-                    }
+                            
+                        }
                         
                         //Let user select between adding a gift via link or manually when they click add gift
                         if (showAddGiftPopup) {
